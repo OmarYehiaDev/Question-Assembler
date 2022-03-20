64b84a7db066d2ee249b35a2771a51a0ad2b74fd
Omar Yehia <omaryahiatawfiek@gmail.com>
2022-03-20 04:32:31 +0200

    ـ

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 // import 'package:questions_assembler/helpers/theme.dart';
 import 'package:questions_assembler/models/subject.dart';
